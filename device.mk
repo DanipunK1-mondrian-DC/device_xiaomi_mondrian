1e0990f6f96a37c0bf193d6d29b9c0b4c7577f3f
DanipunK1 <pegate.eltrueco@gmail.com>
2024-07-14 19:32:31 +0200

    mondrian: include priv keys

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Include priv keys
+-include vendor/lineage-priv/keys/keys.mk
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
